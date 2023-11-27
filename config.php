99df9d88c854738dc58e4b270d907fc28430efc2
Sebastiaan Stok <s.stok@rollerscapes.net>
2023-11-27 11:30:42 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/PdbValidator',
+    'schema_version' => 2,
+];
