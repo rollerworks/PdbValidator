f9ef351c0ff6f92f8ac423e64e602cc65abba532
Sebastiaan Stok <s.stok@rollerscapes.net>
2025-12-15 21:22:57 +0100

    Add Symfony 8 support

--- src/Bundle/DependencyInjection/RollerworksPdbValidatorExtension.php
+++ src/Bundle/DependencyInjection/RollerworksPdbValidatorExtension.php
@@ -17,7 +17,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\Translation\Translator;
 
 final class RollerworksPdbValidatorExtension extends Extension implements PrependExtensionInterface
